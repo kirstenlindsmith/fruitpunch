08185a7717df40db7434699461fac5393e09da8c
kirstenlindsmith <kirstenlindsmith@gmail.com>
2019-03-24 15:36:35 -0400

    added 404 page

---
+++ client/components/NoMatch.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NoMatch = () => {
+  return (
+    <div className="center" id="noMatch">
+      <img src="/assets/404.gif" />
+
+      <h3>Dead link!</h3>
+    </div>
+  )
+}
+
+export default NoMatch
